222fc2f5620d556370684c854f813c94e0813fa6
WishfulGrimm <mattsitunesacct@gmail.com>
2022-06-17 16:42:48 -0400

    Complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    //input validation
+    if (num2 < num1){
+        let temp = num1;
+        num1 = num2;
+        num2 = temp;
+    }
+    if (typeof num1 != "number" || typeof num2 != "number" ||
+            num1 < 0) return 'ERROR';
+    
+    //summation
+    let sum = 0;
+    for (let i = num1; i<=num2; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
