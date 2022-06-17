e5ccbb543d970f77907e0640f02fa31ecd3a0a6f
WishfulGrimm <mattsitunesacct@gmail.com>
2022-06-17 15:07:26 -0400

    Complete exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(s) {
+    if (s === '') return s;
+    let result = [], 
+        holder = [],
+        i = 0;
+    holder = s.split('');
+    for (const c of holder){
+        result.unshift(c);
+    }
+    return result.join('');
 };
 
 // Do not edit below this line
