787e2a1061fa3af4f9a92937d116d6bc8067c0ce
WishfulGrimm <mattsitunesacct@gmail.com>
2022-06-17 17:16:23 -0400

    Complete exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,10 @@
-const ftoc = function() {
-
+const ftoc = function(fah) {
+  let num = (+fah - 32)*(5/9);
+  return +num.toFixed(1);
 };
 
-const ctof = function() {
-
+const ctof = function(cel) {
+  return +((+cel * 1.8) + 32).toFixed(1);
 };
 
 // Do not edit below this line
