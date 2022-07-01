a2b087be6e59b4236b20cbe2122314069813a76c
WishfulGrimm <mattsitunesacct@gmail.com>
2022-07-01 18:19:26 -0400

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(input) {
+    //input validation
+    let num;
+    if (typeof input == 'string') num = +input;
+    else if (+input < 0)return "OOPS";
+    else num = input;
 
+    let sequence = [1, 1];
+    for (let i = 1; sequence.length < num; i++){
+        let next = sequence[i] + sequence[i-1];
+        sequence.push(next);
+    }
+    return sequence.at(-1);
 };
 
 // Do not edit below this line
