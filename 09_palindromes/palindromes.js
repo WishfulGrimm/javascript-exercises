0645d267715e0ea166652d9bff971049309184fb
WishfulGrimm <mattsitunesacct@gmail.com>
2022-07-01 16:04:13 -0400

    Complete palindrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const s = str.replace(/[^a-z0-9]/gi, '').toLowerCase();
+    let checks = (s.length - (s.length % 2)) / 2;
+    for (let i = 0; i < checks; i++){
+        if (s.charAt(i) != s.charAt(s.length - i - 1)) return false;
+    }
+    return true;
 };
 
 // Do not edit below this line
